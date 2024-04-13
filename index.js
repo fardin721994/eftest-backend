482f4ece92aee141caa79e4141779040ee78cb1d
Fardin Kashef <fardinkashef1397@gmail.com>
2024-04-13 19:16:55 +0330

    1403 01 25 authenticaiton added

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+// const bcrypt = require("bcrypt");
 
 const resultsRoutes = require("./routes/results-routes");
+const adminsRoutes = require("./routes/admins-routes");
 
-const HttpError = require("./models/http-error");
+const HttpError = require("./utils/http-error");
 
 const app = express();
+// let hashedPassword;
+// bcrypt.hash("fardin72", 12).then((data) => console.log(data));
 
 app.use(bodyParser.json());
 
 // THE CODE BELOW ABOUT CORS MIGHT NOT BE NEEDED WITHOUT USING REACT-ROUTER-DOM, BUT WHEN USING REACT-ROUTER-DOM, WE NEED IT, OTHERWISE WE GET CORS ERROR IN THE BROWSER.
+// REMEMBER IF YOU TRY TO ACCESS A ROUTE WHICH DOES NOT EXIST, AN ERROR ABOUT CORS POLLICY WILL BE DISPLAYED IN CHROME DEV TOOLS WHICH IS NOT CORRECT AND THE ACTUALL ERROR ABOUT ROUTE NOT BEING FOUND WILL BE DISPLAYED IN BACKEND APP CONSOLE, OKAY? THE ERROR IN CHROME DEV TOOLS IS STH LIKE THIS: "Access to XMLHttpRequest at 'http://localhost:5000/api/adminss/login' from origin 'http://localhost:3000' has been blocked by CORS policy: Response to preflight request doesn't pass access control check: It does not have HTTP ok status."
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -30,15 +36,21 @@ app.use((req, res, next) => {
 // 3) THIS LINE OF CODE MUST BE PLACED AFTER THE ABOVE CODE ABOUT CORS.
 app.use("/api/static-files", express.static("static-files"));
 
-// app.use("/api/courses", coursesRoutes);
-// app.use("/api/users", usersRoutes);
-// app.use("/api/words", wordsRoutes);
-// app.use("/api/products", productsRoutes);
 app.use("/api/results", resultsRoutes);
+app.use("/api/admins", adminsRoutes);
 
-app.use((req, res, next) => {
+app.all("*", (req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
-  throw error;
+  next(error);
+});
+//THIS IS OUR ERROR HANDLING MIDDLEWARE (WHENEVER app.use() RECEIVES A FUNCTION WITH 4 ARGUMENTS (err,req,res,next) => {...} , EXPRESS KNOWS THAT IT IS AN ERROR HANDLING MIDDLE-WARE) 👇:
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 // mongoose
--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,21 +1,28 @@
 const jwt = require("jsonwebtoken");
 
-const HttpError = require("../models/http-error");
+const HttpError = require("../utils/http-error");
 
 module.exports = (req, res, next) => {
   if (req.method === "OPTIONS") {
     return next();
   }
   try {
-    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+    const token = req?.headers?.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
     if (!token) {
-      throw new Error("Authentication failed!");
+      const error = new HttpError(
+        "Authentication failed because token is not provided",
+        401
+      );
+      return next(error);
     }
     const decodedToken = jwt.verify(token, "supersecret_dont_share");
-    req.userData = { userId: decodedToken.userId };
+    req.adminData = { adminId: decodedToken.adminId };
     next();
   } catch (err) {
-    const error = new HttpError("Authentication failed!", 403);
+    const error = new HttpError(
+      "Authentication failed because token is not valid",
+      403
+    );
     return next(error);
   }
 };
